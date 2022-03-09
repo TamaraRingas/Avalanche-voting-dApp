ba830dad0d2d2a48b1b70f147a3f371da7e3b6c1
tamararingas <tamriasdev@gmail.com>
2022-03-09 10:17:50 +0200

    update app.js

--- avalanche-voting/src/App.js
+++ avalanche-voting/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 1. Importing other modules
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      web3: null,
+      account: null,
+      mainInstance: null,
+    };
+  }
+
+  componentDidMount() {
+    this.init();
+  }
+
+  async init() {
+    // 2. Load web3
+    // 3. Load Account
+    // 4. Load Smart-Contract instance
+  }
+
+  render() {
+    return <div>Avalanche evoting application</div>;
+  }
 }
-
 export default App;
